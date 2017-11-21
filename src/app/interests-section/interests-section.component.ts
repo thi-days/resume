34c081d565229cd06454001635e2891b0f6b7e40
Thiago Dias <thiagodbrt@gmail.com>
2017-11-21 15:12:35 -0200

    Added input sections

--- src/app/interests-section/interests-section.component.ts
+++ src/app/interests-section/interests-section.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Interests } from '../models/interests.model';
 
 @Component({
   selector: 'app-interests-section',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./interests-section.component.css']
 })
 export class InterestsSectionComponent implements OnInit {
+
+  @Input()
+  private interestsData: Interests;
 
   constructor() { }
 
